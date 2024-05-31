5b338a2d8e21f0e3bb8984377d4714d22e2ff824
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-31 16:01:35 +0200

    Feat: Created istance 2

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ require_once "Models/movie.php";
 //Creiamo prima istanza
 
 $movie1 = new Movie("Magic Mike", "Usa", "110","2012");
+$movie2 = new Movie("Io sto con gli Ippopotami","Usa","109","1979");
 
 var_dump($movie1);
+
+?>
+
